ef641066ae7e78954118b2568b5459a93a2b2e72
jayteealao_ec2 <jayteealao@gmail.com>
2021-09-08 12:43:05 +0000

    initial commit

---
+++ app/src/main/java/com/example/notekeeper/NoteListFragment.kt
@@ -0,0 +1,40 @@
+package com.example.notekeeper
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.notekeeper.databinding.FragmentNoteListBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class NoteListFragment : Fragment() {
+
+    private var _binding: FragmentNoteListBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentNoteListBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
